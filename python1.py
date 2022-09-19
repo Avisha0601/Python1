6ebd6144993a0abfe021b97cf933bb439cff04a4
Avisha0601 <86399682+Avisha0601@users.noreply.github.com>
2022-09-19 20:41:48 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,22 @@
+count = 0
+while (count < 3):
+    count = count + 1
+    print("Hello Avisha")
+    
+
+n = 4
+for i in range(0, n):
+    print(i)
+    
+
+for letter in 'Avisha':
+    if letter == 'A' or letter == 's':
+        continue
+    print ('Current Letter :', letter)
+    var = 5
+    
+
+fruits = ['apple' , 'orange' , 'kiwi']
+
+for fruit in fruits:
+    print(fruit)
